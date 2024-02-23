ed0a94cd30904eeffd9fcf56def5c5030875120a
Elisandro F07 <f07nobru@gmail.com>
2024-02-23 18:02:49 +0100

    add b2

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,26 @@
 import { router } from "./infra/routes";
 import express from 'express';
 import cors from 'cors';
-require('dotenv').config()
+const B2 = require('b2');
+import Backbone = require('backbone')
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
+const b2 = new B2({
+  applicationKeyId: process.env.B2_APPLICATION_KEY_ID,
+  applicationKey: process.env.B2_APPLICATION_KEY
+});
 
-app.listen(PORT, () => { console.log('Server running')});
+b2.authorize().then(() => {
+  console.log('Autenticado com sucesso');
+  app.listen(PORT, () => { console.log('Server running'); });
+}).catch((err: any) => {
+  console.error('Erro ao autenticar:', err);
+});
